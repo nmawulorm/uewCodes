ea1487c51231af0a2fc66bd81842618e17c8f880
Nicholas Mawulorm <51981537+nmawulorm@users.noreply.github.com>
2023-08-21 23:02:36 +0000

    Create adb_commands.bat

---
+++ adb_commands.bat
@@ -0,0 +1,5 @@
+REM To lock the screen:
+adb shell input keyevent 82 && adb shell input keyevent 26 && adb shell input keyevent 26
+
+REM To unlock the screen without pass
+adb shell input keyevent 82 && adb shell input keyevent 66
