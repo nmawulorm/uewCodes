3d0334d3bcdb4a6d732238b99d11cde24c969554
Nicholas Mawulorm <51981537+nmawulorm@users.noreply.github.com>
2024-07-24 10:18:33 +0000

    Update adb_commands.bat

--- adb_commands.bat
+++ adb_commands.bat
@@ -1,5 +1,5 @@
 REM To lock the screen:
 adb shell input keyevent 82 && adb shell input keyevent 26 && adb shell input keyevent 26
 
-REM To unlock the screen without pass
+REM To unlock the screen without touching screen
 adb shell input keyevent 82 && adb shell input keyevent 66
